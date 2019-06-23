23c08b4b45999e69ab0916d6b43a5e28113c53a2
Christopher Hiller <boneskull@boneskull.com>
2019-06-22 20:48:02 -0700

    wip: deep sleep via DS3231 RTC
    todo: the alarm pin is high and goes low when the alarm is triggered.
    but the device only wakes from sleep when the pin is high, so, I'll need
    to invert the signal, or use something else. 555?

--- src/particle-dht22.cpp
+++ src/particle-dht22.cpp
@@ -1,7 +1,9 @@
 
 #include "ArduinoJson.h"
 #include "Adafruit_DHT.h"
+#include "DS3231_Simple.h"
 #include "Particle.h"
+#include <time.h>
 
 SYSTEM_MODE(AUTOMATIC);
 
@@ -12,9 +14,9 @@ SYSTEM_MODE(AUTOMATIC);
 #define DEFAULT_DELAY 900000
 #define DEFAULT_ENABLE true
 #define DEFAULT_DEEP_SLEEP false
-#define FIRMWARE_VERSION 5
+#define FIRMWARE_VERSION 6
 #define FIRMWARE_NAME "particle-dht22"
-#define STARTUP_DELAY 3000
+#define STARTUP_DELAY 1000
 
 struct Config {
   uint8_t version;
@@ -34,7 +36,9 @@ int disableDeepSleep(String ignored);
 int setDelay(String ms);
 void publish(String topic, String data);
 void readDHT();
+void adjustRTC();
 
+DS3231_Simple rtc;
 Config defaultConfig = {FIRMWARE_VERSION, DEFAULT_DELAY, DEFAULT_ENABLE,
                         DEFAULT_DEEP_SLEEP};
 Config config;
@@ -48,7 +52,10 @@ unsigned long prevMillis = 0;
 
 void setup() {
   Log.info("%s on Device OS v%s", FIRMWARE_NAME, System.version().c_str());
-
+  Log.trace("Syncing time");
+  Particle.syncTime();
+  waitUntil(Particle.syncTimeDone);
+  Log.trace("Checking if firmware is up-to-date");
   firmwareCheck();
 
   Particle.function("enable", enable);
@@ -59,129 +66,170 @@ void setup() {
   Particle.variable("config", configJson);
 
   Log.info("Config: %s", configJson);
-  dht.begin();
-  Log.trace("Performing initial read");
-  delay(STARTUP_DELAY);
 
+  dht.begin();
+  rtc.begin();
+  adjustRTC();
+  rtc.disableAlarms();
+  delay(STARTUP_DELAY);
+  Log.trace("Performing initial read");
   readDHT();
   prevMillis = millis();
 }
 
+time_t timestamp(DateTime dt) {
+  struct tm t = {.tm_sec = dt.Second,
+                 .tm_min = dt.Minute,
+                 .tm_hour = dt.Hour,
+                 .tm_mday = dt.Day,
+                 .tm_mon = dt.Month - 1,
+                 .tm_year = dt.Year + 100};
+  return mktime(&t);
+}
+
+void adjustRTC() {
+  DateTime deviceNow = {.Second = uint8_t(Time.second()),
+                        .Minute = uint8_t(Time.minute()),
+                        .Hour = uint8_t(Time.hour()),
+                        .Dow = uint8_t(Time.weekday()),
+                        .Day = uint8_t(Time.day()),
+                        .Month = uint8_t(Time.month()),
+                        .Year = uint8_t(Time.year() - 2000)};
+  rtc.write(deviceNow);
+  DateTime now = rtc.read();
+  Log.info("Set RTC time to %s",
+           Time.format(timestamp(now), TIME_FORMAT_ISO8601_FULL).c_str());
+}
+
+void setRTCAlarm() {}
+
 void loop() {
   if (config.enabled) {
     if (config.deepSleep) {
-      Log.info("Sleeping for %dms", config.delayMs);
-      System.sleep({D1}, RISING, config.delayMs / 1000);
-      readDHT();
-    } else {
-      unsigned long currentMillis = millis();
-      if (currentMillis - prevMillis >= config.delayMs) {
+      rtc.setAlarm({DS3231_Simple::ALARM_EVERY_SECOND);
+        Log.info("Sleeping for %dms", config.delayMs);
+        System.sleep({D8}, FALLING, config.delayMs / 1000);
         readDHT();
-        prevMillis = currentMillis;
-      }
+        rtc.checkAlarms();
+    } else {
+        unsigned long currentMillis = millis();
+        if (currentMillis - prevMillis >= config.delayMs) {
+          readDHT();
+          prevMillis = currentMillis;
+        }
     }
-  } else if (config.deepSleep) {
-    Log.warn("Call setEnabled() to enable deep sleep");
-    publish("warning", "Call setEnabled() to enable deep sleep");
+    } else if (config.deepSleep) {
+      Log.warn("Call setEnabled() to enable deep sleep");
+      publish("warning", "Call setEnabled() to enable deep sleep");
+    }
+    // if (rtc.lostPower()) {
+    //   Log.warn("RTC lost power and needs to be adjusted!");
+    //   adjustRTC();
+    // }
   }
-}
 
-void readDHT() {
-  float h = dht.getHumidity();
-  float t = dht.getTempCelcius();
-  float f = dht.getTempFarenheit();
-  float k = dht.getTempKelvin();
-  float dp = dht.getDewPoint();
+  void readDHT() {
+    float h = dht.getHumidity();
+    float t = dht.getTempCelcius();
+    float f = dht.getTempFarenheit();
+    float k = dht.getTempKelvin();
+    float dp = dht.getDewPoint();
 
-  Log.info("Humid: %.2f%% - Temp: %.2f*C / %.2f*F / %.2f*K - DewP: %.2f*C", h,
-           t, f, k, dp);
+    Log.info("Humid: %.2f%% - Temp: %.2f C / %.2f F / %.2f K - DewP: %.2f C", h,
+             t, f, k, dp);
 
-  publish("humidity", String::format("%.2f", h));
-  publish("temperature", String::format("%.2f", f));
-  publish("dew_point", String::format("%.2f", dp));
-}
+    publish("humidity", String::format("%.2f", h));
+    publish("temperature", String::format("%.2f", f));
+    publish("dew_point", String::format("%.2f", dp));
+  }
 
-void firmwareCheck() {
-  EEPROM.get(0, config);
-  if (config.version == FIRMWARE_VERSION) {
+  void firmwareCheck() {
+    EEPROM.get(0, config);
+    if (config.version == FIRMWARE_VERSION) {
+      serializeConfig();
+    } else {
+      resetConfig();
+    }
+  }
+
+  void resetConfig() {
+    Log.warn("Firmware out of date; resetting config");
+    config = defaultConfig;
+    saveConfig();
+  }
+
+  size_t serializeConfig() {
+    doc["delay"] = config.delayMs;
+    doc["deepSleep"] = config.deepSleep;
+    doc["enabled"] = config.enabled;
+    return serializeJson(doc, configJson);
+  }
+
+  void saveConfig() {
+    EEPROM.put(0, config);
     serializeConfig();
-  } else {
-    resetConfig();
+    publish("config", configJson);
   }
-}
 
-void resetConfig() {
-  Log.warn("Firmware out of date; resetting config");
-  config = defaultConfig;
-  saveConfig();
-}
-
-size_t serializeConfig() {
-  doc["delay"] = config.delayMs;
-  doc["deepSleep"] = config.deepSleep;
-  doc["enabled"] = config.enabled;
-  return serializeJson(doc, configJson);
-}
-
-void saveConfig() {
-  EEPROM.put(0, config);
-  serializeConfig();
-  publish("config", configJson);
-}
-
-int setDelay(String value) {
-  unsigned int delayMs = value.toInt();
-  if (delayMs == 0) {
-    Log.warn("setDelay() called with non-integer or non-positive value: %s",
-             value.c_str());
-    publish("error",
-            String::format(
-                "setDelay() called with non-integer or non-positive value: %s",
-                value.c_str()));
+  int setDelay(String value) {
+    unsigned int delayMs = value.toInt();
+    if (delayMs == 0) {
+      Log.warn("setDelay() called with non-integer or non-positive value: %s",
+               value.c_str());
+      publish(
+          "error",
+          String::format(
+              "setDelay() called with non-integer or non-positive value: %s",
+              value.c_str()));
+      return 0;
+    }
+    if (delayMs != config.delayMs) {
+      config.delayMs = delayMs;
+      saveConfig();
+      return 1;
+    }
     return 0;
   }
-  if (delayMs != config.delayMs) {
-    config.delayMs = delayMs;
-    return 1;
-  }
-  return 0;
-}
 
-int enable(String ignored = "") {
-  if (!config.enabled) {
-    config.enabled = true;
-    return 1;
+  int enable(String ignored = "") {
+    if (!config.enabled) {
+      config.enabled = true;
+      saveConfig();
+      return 1;
+    }
+    return 0;
   }
-  return 0;
-}
 
-int disable(String ignored = "") {
-  if (config.enabled) {
-    config.enabled = false;
-    return 1;
+  int disable(String ignored = "") {
+    if (config.enabled) {
+      config.enabled = false;
+      saveConfig();
+      return 1;
+    }
+    return 0;
   }
-  return 0;
-}
 
-int enableDeepSleep(String ignored = "") {
-  if (!config.deepSleep) {
-    config.deepSleep = true;
-    return 1;
+  int enableDeepSleep(String ignored = "") {
+    if (!config.deepSleep) {
+      config.deepSleep = true;
+      saveConfig();
+      return 1;
+    }
+    return 0;
   }
-  return 0;
-}
 
-int disableDeepSleep(String ignored = "") {
-  if (config.deepSleep) {
-    config.deepSleep = false;
-    return 1;
+  int disableDeepSleep(String ignored = "") {
+    if (config.deepSleep) {
+      config.deepSleep = false;
+      saveConfig();
+      return 1;
+    }
+    return 0;
   }
-  return 0;
-}
 
-void publish(String topic, String data) {
-  String fullTopic = String(PUBLISH_NAMESPACE) + "/" +
-                     String(PUBLISH_DEVICE_NAME) + "/" + topic;
-  Log.trace("PUBLISH <%s>: %s", fullTopic.c_str(), data.c_str());
-  Particle.publish(fullTopic, data, PRIVATE);
-}
+  void publish(String topic, String data) {
+    String fullTopic = String(PUBLISH_NAMESPACE) + "/" +
+                       String(PUBLISH_DEVICE_NAME) + "/" + topic;
+    Log.trace("PUBLISH <%s>: %s", fullTopic.c_str(), data.c_str());
+    Particle.publish(fullTopic, data, PRIVATE);
+  }
